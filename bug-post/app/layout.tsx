294474d34bc8734c33514cee1a0c068d96032dbb
Sri Hari <harigallery9@gmail.com>
2025-05-23 23:06:11 +0530

    fixed google font error

--- bug-post/app/layout.tsx
+++ bug-post/app/layout.tsx
@@ -1,18 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navigation from "./components/Navigation";
-import Link from "next/link";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Stack Overflow",
@@ -27,7 +15,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navigation></Navigation>
         {children}
