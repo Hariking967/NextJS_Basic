abfaef957a9b515d6eda94dbcacb82c4c4a595a8
Sri Hari <harigallery9@gmail.com>
2025-05-28 19:45:04 +0530

    css changes in bug page

--- bug-post/app/components/BugPage.tsx
+++ bug-post/app/components/BugPage.tsx
@@ -7,12 +7,12 @@ export default function BugPage() {
     let bugDatas = data.bugDatas;
     let bugContent = bugDatas.map(bug=>(
         <Link key={bug.id} href= {`/bug/${bug.id}`}>
-            <div className='flex flex-row'>
+            <div className='flex flex-row gap-4 p-4 m-3 rounded-xl bg-gray-700 hover:bg-gray-600 transition-all shadow-md cursor-pointer'>
                 <p>{bug.answered} answered</p>
-                <div className='flex flex-col items-start md:items-center justify-between gap-4 p-4 m-3 rounded-xl bg-gray-700 hover:bg-gray-600 transition-all shadow-md cursor-pointer'>
+                <div className='flex flex-col w-[90vw]'>
                     <p className='text-3xl m-3 truncate'>{bug.title}</p>
-                    <p className='text-3xl m-3'>{bug.desc}</p>
-                    <p className='text-3xl m-3'>{getUserById(bug.userid)}</p>
+                    <p className='text-3xl m-3 truncate'>{bug.desc}</p>
+                    <p className='text-3xl m-3'>Posted by: {getUserById(bug.userid)}</p>
                 </div>
             </div>
             
